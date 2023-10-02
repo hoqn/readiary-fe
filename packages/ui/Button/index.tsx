812ccfde87c1b839eae3a76764df5c411547e885
Hogyun Jeon <akzm9999@gmail.com>
2023-10-02 22:36:59 +0900

    [Feat] UI Polymorphism 구현

--- packages/ui/Button/index.tsx
+++ packages/ui/Button/index.tsx
@@ -1,7 +1,18 @@
-import s from './button.module.scss';
+import type { PropsWithChildren } from "react";
+import { polymorphicForwardRef } from "../utils/polymorphic";
+import s from "./button.module.scss";
 
-export function Button() {
-  return (
-    <span>HELLO</span>
-  );
+export interface ButtonProps extends PropsWithChildren {
+  icon?: string;
+  className?: string;
 }
+
+export const Button = polymorphicForwardRef<"button", ButtonProps>(({ children, className, as, ...restProps }, ref) => {
+  const Wrapper = as || "button";
+
+  return (
+    <Wrapper className={`${className} ${s.button}`} ref={ref} type="button" {...restProps}>
+      {children}
+    </Wrapper>
+  );
+});
