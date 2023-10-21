dda9625f569d2298a51a483dd490c3e60f28536d
Hogyun Jeon <akzm9999@gmail.com>
2023-10-21 20:02:18 +0900

    [Feat] Loading Indicator 효과 변경

--- src/components/ui/LoadingIndicator/LoadingIndicator.tsx
+++ src/components/ui/LoadingIndicator/LoadingIndicator.tsx
@@ -1,9 +1,8 @@
 import $ from "./LoadingIndicator.module.scss";
 
 function LoadingIndicator() {
-  return (
-    <div className={$.loadingIndicator} />
-  )
+  // return <div className={$.loadingIndicator} />;
+  return <div className={$.ellipsis}><div></div><div></div><div></div><div></div></div>;
 }
 
 export default LoadingIndicator;
