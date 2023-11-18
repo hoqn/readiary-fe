6de00caa6c31a1543c488d6d0ccc19d0f60d3e7e
Hogyun Jeon <akzm9999@gmail.com>
2023-11-18 19:12:06 +0900

    feat: write base api service class

---
+++ src/services/base.service.ts
@@ -0,0 +1,5 @@
+export default abstract class BaseApiService {
+  // protected BASE_URL: string = "/api";
+  protected BASE_URL: string = "http://localhost:3000/api"; // (임시) Server Component에선 full url이 필요
+  // TODO: URL 수정 
+}
