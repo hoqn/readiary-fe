de5b70f1684baccc7622c7f28b9d685114c3bae4
Hogyun Jeon <akzm9999@gmail.com>
2023-12-04 13:51:17 +0900

    update: rating stars component
    rating stars can be edited

--- src/components/ui/rating-stars/rating-stars.tsx
+++ src/components/ui/rating-stars/rating-stars.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import cs from "classnames";
 import styles from "./rating-stars.module.scss";
 
 import IcHalfStar from "@material-symbols/svg-400/rounded/star_rate_half-fill.svg";
 import IcFullStar from "@material-symbols/svg-400/rounded/star_rate-fill.svg";
 import IcEmptyStar from "@material-symbols/svg-400/rounded/star_rate.svg";
+import { MouseEventHandler, TouchEventHandler, useCallback, useEffect, useRef, useState } from "react";
 
 function Star({ value }: { value: number }) {
   if (value >= 0) return <IcFullStar />;
@@ -14,18 +17,63 @@ function Star({ value }: { value: number }) {
 interface Props extends BaseProps {
   value: number;
   isEditable?: boolean;
-  onEdit?: (value: number | null) => boolean;
+  onEdit?: (value: number | null) => void;
 }
 
 export default function RatingStars({ className, value, isEditable, onEdit, ...restProps }: Props) {
+  const [displayValue, setDisplayValue] = useState<number>(value);
+  const [isEditing, setEditing] = useState<boolean>(false);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  const doOnMouseMove: MouseEventHandler<HTMLDivElement> = useCallback((e) => {
+    e.preventDefault();
+
+    if (isEditing && ref.current) {
+      const bounding = ref.current.getBoundingClientRect();
+      setDisplayValue(Math.round((10 * (e.pageX - bounding.x)) / bounding.width));
+    }
+  }, [isEditing]);
+  
+  const doOnMouseDown: MouseEventHandler = useCallback((e) => {
+    e.preventDefault();
+    setEditing(true);
+    if (ref.current) {
+      const bounding = ref.current.getBoundingClientRect();
+      setDisplayValue(Math.round((10 * (e.pageX - bounding.x)) / bounding.width));
+    }
+  }, []);
+  
+  const doOnMouseUp: MouseEventHandler = useCallback((e) => {
+    e.preventDefault();
+    setEditing(false);
+    if (ref.current) {
+      const bounding = ref.current.getBoundingClientRect();
+      const value = Math.round((10 * (e.pageX - bounding.x)) / bounding.width);
+
+      setDisplayValue(value);
+      onEdit && onEdit(value);
+    }
+  }, []);
+
+  useEffect(() => {
+    setDisplayValue(value);
+  }, [value]);
+
   return (
-    <div className={cs(styles["root"], className)} {...restProps}>
-      <div className={styles["inner"]}>
-        <Star value={value - 2} />
-        <Star value={value - 4} />
-        <Star value={value - 6} />
-        <Star value={value - 8} />
-        <Star value={value - 10} />
+    <div className={cs(styles["root"], isEditing && styles["root--editing"], className)} {...restProps}>
+      <div
+        className={styles["inner"]}
+        onMouseMove={isEditable ? doOnMouseMove : undefined}
+        onMouseDown={isEditable ? doOnMouseDown : undefined}
+        onMouseUp={isEditable ? doOnMouseUp : undefined}
+        ref={ref}
+      >
+        <Star value={displayValue - 2} />
+        <Star value={displayValue - 4} />
+        <Star value={displayValue - 6} />
+        <Star value={displayValue - 8} />
+        <Star value={displayValue - 10} />
       </div>
     </div>
   );
