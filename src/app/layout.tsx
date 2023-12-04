2a8ff9cb25cd90152a0ad6b9d2cf7c0748ad8b4f
Hogyun Jeon <akzm9999@gmail.com>
2023-12-04 14:59:13 +0900

    update: set manifest.json & prevent context menu
    for PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,31 @@ import type { Metadata } from "next";
 import "@/styles/global.scss";
 import Providers from "./_providers";
 import styles from "./common.module.scss";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Readiary",
   description: "Let's read!",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
-      <body className={styles.body}>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
+    <>
+      <Script id="context-menu">
+        {`
+          window.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            return false;
+          });
+        `}
+      </Script>
+      <html lang="ko">
+        <body className={styles.body}>
+          <Providers>{children}</Providers>
+        </body>
+      </html>
+    </>
   );
 }
