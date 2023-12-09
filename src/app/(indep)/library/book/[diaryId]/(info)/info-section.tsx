d4ec36a953401e6723810be42c706489805fa118
Hogyun Jeon <akzm9999@gmail.com>
2023-12-09 16:50:30 +0900

    fix: hide rating & takeaway before reading done

--- src/app/(indep)/library/book/[diaryId]/(info)/info-section.tsx
+++ src/app/(indep)/library/book/[diaryId]/(info)/info-section.tsx
@@ -7,6 +7,8 @@ import { useLocalContext } from "../context";
 import styles from "./info-section.module.scss";
 import Rating from "./rating";
 import Status from "./status";
+import { ReadingStatus } from "@/services/api/diary.api";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function InfoSection() {
   const pathname = usePathname();
@@ -16,20 +18,26 @@ export default function InfoSection() {
   if (!diaryDetail) return;
 
   const {
-    bookDiary: { takeaway },
+    bookDiary: { takeaway, readingStatus },
   } = diaryDetail;
 
   return (
     <Section className={styles["info-section"]}>
       <Status />
-      <Rating />
-      <Link className={styles["takeaway-wrapper"]} href={`${pathname}/take`}>
-        {takeaway?.length ? (
-          <div className={styles["takeaway"]}>{takeaway}</div>
-        ) : (
-          <div className={styles["takeaway-empty"]}>감상평을 남겨주세요!</div>
+      <AnimatePresence mode="popLayout">
+        {readingStatus === ReadingStatus.AFTER && (
+          <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0 }}>
+            <Rating />
+            <Link className={styles["takeaway-wrapper"]} href={`${pathname}/take`}>
+              {takeaway?.length ? (
+                <div className={styles["takeaway"]}>{takeaway}</div>
+              ) : (
+                <div className={styles["takeaway-empty"]}>감상평을 남겨주세요!</div>
+              )}
+            </Link>
+          </motion.div>
         )}
-      </Link>
+      </AnimatePresence>
     </Section>
   );
 }
