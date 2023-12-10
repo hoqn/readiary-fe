f47b30998bb07197b749d8b0e42236bcc2b919d1
Hogyun Jeon <akzm9999@gmail.com>
2023-12-10 15:01:23 +0900

    fix: pretendard font-family as local

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,18 @@ import "@/styles/global.scss";
 import Providers from "./_providers";
 import styles from "./common.module.scss";
 import Script from "next/script";
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "Readiary",
   description: "Let's read!",
   manifest: "/manifest.json",
 };
+
+const pretendardVariable = localFont({
+  src: "./PretendardVariable.woff2",
+  display: "swap",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -22,7 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           });
         `}
       </Script>
-      <html lang="ko">
+      <html lang="ko" className={pretendardVariable.className}>
         <body className={styles.body}>
           <Providers>{children}</Providers>
         </body>
