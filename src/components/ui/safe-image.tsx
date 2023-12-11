776b909b8907f49d9e479de1ecd542eebec71788
Hogyun Jeon <akzm9999@gmail.com>
2023-12-11 13:48:06 +0900

    fix: unoptimize images

--- src/components/ui/safe-image.tsx
+++ src/components/ui/safe-image.tsx
@@ -5,13 +5,13 @@ import { useCallback } from "react";
 
 interface Props extends Omit<ImageProps, "onError"> {}
 
-export default function SafeImage({ alt, width, height, ...restProps }: Props) {
+export default function SafeImage({ alt, width, height, unoptimized = true, ...restProps }: Props) {
   const fallbackImgUrl = `https://placehold.co/${width}x${height}/E9F6E9/2A7E38?text=Readiary`;
   const doOnError: React.ReactEventHandler<HTMLImageElement> = useCallback((e) => {
     e.currentTarget.src = fallbackImgUrl;
   }, [fallbackImgUrl]);
 
   return (
-    <Image alt={alt} width={width} height={height} blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mPs/A8AAhcBiiT020IAAAAASUVORK5CYII=" {...restProps} onError={doOnError} />
+    <Image unoptimized={unoptimized} alt={alt} width={width} height={height} blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mPs/A8AAhcBiiT020IAAAAASUVORK5CYII=" {...restProps} onError={doOnError} />
   )
 }
